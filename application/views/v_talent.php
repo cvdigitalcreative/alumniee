24a52e5367f2c99b2be2499095db46dcef39fa92
Malian Zikri <malianzikri92@gmail.com>
2019-06-12 19:48:01 +0700

    fixing talent

--- application/views/v_talent.php
+++ application/views/v_talent.php
@@ -7,10 +7,10 @@
 	      	<img class="one-third js-fullheight align-self-end order-md-last img-fluid" src="<?= base_url()?>assets/images/undraw_pair_programming_njlp.svg" alt="">
 	        <div class="one-forth d-flex align-items-center ftco-animate js-fullheight">
 	        	<div class="text mt-5">
-	        		<span class="subheading">Isometric Hosting</span>
-	            <h1 class="mb-3"><span>Design,</span> <span>Development,</span> <span>Hosting</span></h1>
-	            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-	            <p><a href="#" class="btn btn-primary px-4 py-3">Get in touch</a></p>
+	        		
+	            <h1 class="mb-3">Temukan Karir Sesuai Impian Anda</h1>
+	            <p>Alumne hadir untuk membantu anda dalam menemukan karir yang sesuai dengan personal diri anda</p>
+	            <p><a href="#" class="btn btn-primary px-4 py-3">Cari Pekerjaan</a></p>
 	          </div>
 	        </div>
 	    	</div>
@@ -22,8 +22,8 @@
       <div class="container">
       	<div class="row justify-content-center mb-5 pb-3">
           <div class="col-md-7 text-center heading-section ftco-animate">
-            <h2 class="mb-4">Why Choose Us</h2>
-            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+            <h2 class="mb-4">Why Chooose Us</h2>
+            <p>Alumne mempermudah anda dalam mencari pekerjaan yang sesuai cukup hanya memasukan profile anda</p>
           </div>
         </div>
         <div class="row">
@@ -33,8 +33,8 @@
             		<span class="flaticon-cloud"></span>
             	</div>
               <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
-                <h3 class="heading">Super Fast Server</h3>
-                <p class="mb-0">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <h3 class="heading">Free</h3>
+                <p class="mb-0">Anda tidak akan dikenakan biaya apapun</p>
               </div>
             </div>      
           </div>
@@ -44,8 +44,8 @@
             		<span class="flaticon-server"></span>
             	</div>
               <div class="media-body pl-4">
-                <h3 class="heading">Daily Backups</h3>
-                <p class="mb-0">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <h3 class="heading">Simple</h3>
+                <p class="mb-0">Kami hanya akan menanyakan informasi yang dibutuhkan untuk mencocokan pekerjaan yang sesuai</p>
               </div>
             </div>    
           </div>
@@ -55,109 +55,19 @@
             		<span class="flaticon-customer-service"></span>
             	</div>
               <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
-                <h3 class="heading">Technical Services</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <h3 class="heading">Otomatis</h3>
+                <p>Anda akan mendapatkan lowongan pekerjaan yang sesuai dengan anda tanpa anda melakukan pencarian</p>
               </div>
             </div>      
           </div>
-					<div class="col-md-6 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services d-flex align-items-center">
-            	<div class="icon d-flex align-items-center justify-content-center">
-            		<span class="flaticon-life-insurance"></span>
-            	</div>
-              <div class="media-body pl-4">
-                <h3 class="heading">Secure and Reliable</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-              </div>
-            </div>      
-          </div>
-          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services d-flex align-items-center">
-            	<div class="icon d-flex align-items-center justify-content-center order-md-last">
-            		<span class="flaticon-cloud-computing"></span>
-            	</div>
-              <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
-                <h3 class="heading">DNS Control</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-              </div>s
-            </div>    
-          </div>
-          <div class="col-md-6 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services d-flex align-items-center">
-            	<div class="icon d-flex align-items-center justify-content-center">
-            		<span class="flaticon-settings"></span>
-            	</div>
-              <div class="media-body pl-4">
-                <h3 class="heading">24/7 Support</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-              </div>
-            </div>      
-          </div>
+			
+          
+          
         </div>
       </div>
     </section>
 
-    <section class="ftco-section ftco-no-pt ftc-no-pb">
-    	<div class="container">
-    		<div class="row">
-    			<div class="col-lg-6 py-5">
-    				<img src="<?= base_url()?>assets/images/undraw_podcast_q6p7.svg" class="img-fluid" alt="">
-    				<div class="heading-section ftco-animate mt-5">
-	            <h2 class="mb-4">Our Main Services</h2>
-	            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
-	          </div>
-    			</div>
-    			<div class="col-lg-6 py-5">
-    				<div class="row">
-    					<div class="col-md-6 ftco-animate">
-    						<div class="media block-6 services border text-center">
-		            	<div class="icon d-flex align-items-center justify-content-center">
-		            		<span class="flaticon-cloud-computing"></span>
-		            	</div>
-		              <div class="mt-3 media-body media-body-2">
-		                <h3 class="heading">Cloud VPS</h3>
-		                <p>Even the all-powerful Pointing has no control about the blind texts</p>
-		              </div>
-		            </div>
-    					</div>
-    					<div class="col-md-6 ftco-animate">
-    						<div class="media block-6 services border text-center">
-		            	<div class="icon d-flex align-items-center justify-content-center">
-		            		<span class="flaticon-cloud"></span>
-		            	</div>
-		              <div class="mt-3 media-body media-body-2">
-		                <h3 class="heading">Share</h3>
-		                <p>Even the all-powerful Pointing has no control about the blind texts</p>
-		              </div>
-		            </div>
-    					</div>
-    					<div class="col-md-6 ftco-animate">
-    						<div class="media block-6 services border text-center">
-		            	<div class="icon d-flex align-items-center justify-content-center">
-		            		<span class="flaticon-server"></span>
-		            	</div>
-		              <div class="mt-3 media-body media-body-2">
-		                <h3 class="heading">VPS</h3>
-		                <p>Even the all-powerful Pointing has no control about the blind texts</p>
-		              </div>
-		            </div>
-    					</div>
-    					<div class="col-md-6 ftco-animate">
-    						<div class="media block-6 services border text-center">
-		            	<div class="icon d-flex align-items-center justify-content-center">
-		            		<span class="flaticon-database"></span>
-		            	</div>
-		              <div class="mt-3 media-body media-body-2">
-		                <h3 class="heading">Dedicated</h3>
-		                <p>Even the all-powerful Pointing has no control about the blind texts</p>
-		              </div>
-		            </div>
-    					</div>
-    				</div>
-          </div>
-    		</div>
-    	</div>
-    </section>
+    
 
     <section class="ftco-section bg-light">
     	<div class="container">
@@ -170,11 +80,11 @@
     		<div class="row">
           <div class="col-md-12 nav-link-wrap mb-5 pb-md-5 pb-sm-1 ftco-animate">
             <div class="nav ftco-animate nav-pills justify-content-center text-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <a class="nav-link active" id="v-pills-nextgen-tab" data-toggle="pill" href="#v-pills-nextgen" role="tab" aria-controls="v-pills-nextgen" aria-selected="true">Next gen VPS</a>
+              <a class="nav-link active" id="v-pills-nextgen-tab" data-toggle="pill" href="#v-pills-nextgen" role="tab" aria-controls="v-pills-nextgen" aria-selected="true">Masukan Data Diri Anda</a>
 
-              <a class="nav-link" id="v-pills-performance-tab" data-toggle="pill" href="#v-pills-performance" role="tab" aria-controls="v-pills-performance" aria-selected="false">Performance</a>
+              <a class="nav-link" id="v-pills-performance-tab" data-toggle="pill" href="#v-pills-performance" role="tab" aria-controls="v-pills-performance" aria-selected="false">Terima Pekerjaan</a>
 
-              <a class="nav-link" id="v-pills-effect-tab" data-toggle="pill" href="#v-pills-effect" role="tab" aria-controls="v-pills-effect" aria-selected="false">Effectiveness</a>
+              <a class="nav-link" id="v-pills-effect-tab" data-toggle="pill" href="#v-pills-effect" role="tab" aria-controls="v-pills-effect" aria-selected="false">Dapatkan Pekerjaan Impian Anda</a>
             </div>
           </div>
           <div class="col-md-12 align-items-center ftco-animate">
@@ -187,10 +97,9 @@
 	              		<img src="<?= base_url()?>assets/images/undraw_laravel_and_vue_59tp.svg" class="img-fluid" alt="">
 	              	</div>
 	              	<div class="one-half ml-md-5 align-self-center">
-		                <h2 class="mb-4">Next gen VPS hosting</h2>
-		              	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-		                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-		                <p><a href="#" class="btn btn-primary py-3">Get in touch</a></p>
+		                <h2 class="mb-4">Masukan Data Diri Anda</h2>
+		              	<p>Anda akan diminta mengisi beberapa data yang akan digunakan untuk mencocokan anda dengan perusahaan yang sesuai dengan kriteria.</p>
+		               
 		              </div>
 	              </div>
               </div>
@@ -201,10 +110,9 @@
 	              		<img src="images/undraw_visual_data_b1wx.svg" class="img-fluid" alt="">
 	              	</div>
 	              	<div class="one-half order-first mr-md-5 align-self-center">
-		                <h2 class="mb-4">Performance VPS hosting</h2>
-		              	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-		                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-		                <p><a href="#" class="btn btn-primary py-3">Get in touch</a></p>
+		                <h2 class="mb-4">Terima Pekerjaan</h2>
+		              	<p>Anda akan memilih pekerjaan mana yang anda akan lamar dimana pekerjaan yang muncul telah difilter terlebih dahulu oleh sistem sesuai dengan data diri anda</p>
+		               
 		              </div>
 	              </div>
               </div>
@@ -215,10 +123,9 @@
 	              		<img src="images/undraw_business_plan_5i9d.svg" class="img-fluid" alt="">
 	              	</div>
 	              	<div class="one-half ml-md-5 align-self-center">
-		                <h2 class="mb-4">Effective VPS hosting</h2>
-		              	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-		                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-		                <p><a href="#" class="btn btn-primary py-3">Get in touch</a></p>
+		                <h2 class="mb-4">Dapatkan Pekerjaan Impian Anda</h2>
+		              	<p>Setelah semua proses anda lalui anda akan mendapatkan pekerjaan yang anda impinkan</p>
+		               
 		              </div>
 	              </div>
               </div>
